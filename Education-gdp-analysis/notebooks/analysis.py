ec88f2c36d08ada3d38b094323968ee42e97d120
lajan <aiswarialajan124@gmail.com>
2026-03-23 18:35:01 +0000

    Started project 02

--- Education-gdp-analysis/notebooks/analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-file_path = "../data/P_Data_Extract_From_World_Development_Indicators (1).xlsx"
-
-df = pd.read_excel(file_path)
-print(df.head())
-
-print(df["Series Name"].unique())
